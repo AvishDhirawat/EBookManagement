8c847e0862b99dca23e32c783b42dadeeb26622c
Avish Dhirawat <31956212+Avishdhirawat0@users.noreply.github.com>
2021-07-20 10:40:25 +0530

    Change Passw to root

--- src/main/java/com/DB/DBConnect.java
+++ src/main/java/com/DB/DBConnect.java
@@ -8,7 +8,7 @@ public class DBConnect {
 	public static Connection getConn() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/ebookapp","root","Avish@0709");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/ebookapp","root","root");
 			
 			} catch(Exception e) {
 				e.printStackTrace();
